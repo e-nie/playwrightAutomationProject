e68bf98067226f737cb147d2bfe72029fee91211
e-nie <eniehaus111@gmail.com>
2024-06-18 01:15:25 +0200

    add some tests

--- tests/formLayouts.spec.ts
+++ tests/formLayouts.spec.ts
@@ -1,16 +1,29 @@
 import { test, expect } from '@playwright/test';
- 
-test.describe('FORM LAYOUTS', () => { 
 
-
-    test('Fill all fields', async ({ page }) => {
+test.describe('FORM LAYOUTS', () => {
+    test.beforeEach(async ({ page }) => {
         await page.goto("http://localhost:4200/pages/forms/layouts")
+    })
+
+    test.only('Inline form', async ({ page }) => {
+    // await page.goto("http://localhost:4200/pages/forms/layouts")
         await page.locator('input[placeholder="Jane Doe"]').fill('Evchen Wonderful')
         await page.locator('input[placeholder="Email"]').nth(0).fill('EvchenWonderful@Brilliance.com')
         await page.locator('span.custom-checkbox').nth(0).check()
         await page.locator('button[status="primary"]').nth(0).click()
+        await page.pause()
+    })
+    test('Using the grid', async ({ page }) => {
+    // await page.goto("http://localhost:4200/pages/forms/layouts")
         await page.locator('#inputEmail1').fill("Evchen@kukaryamba.com")
         await page.locator('#inputPassword2').fill("uzhasno")
-        // await page.locator('[class^="native-input"][type="radio"]').first().check();
+        await page.locator('[class="inner-circle"]').nth(0).check();
+        await page.locator('[class="inner-circle"]').nth(1).check();
+        await page.locator('button[status="primary"]').nth(1).click()
     })
-    })
+
+
+
+
+})
+
--- tests/formPage.spec.ts
+++ tests/formPage.spec.ts
@@ -1,4 +1,4 @@
-import { test as it} from '@playwright/test'
+import { test as it } from '@playwright/test'
 /*
 it.describe('FORM PAGE', () => {
     it.beforeAll(() => {
@@ -33,27 +33,27 @@ it.describe('FORM PAGE', () => {
 
 
 it.describe('FORM PAGE TYPE', () => {
-    it('Fill all fields', async ({page}) => {
+    it('Fill all fields', async ({ page }) => {
         await page.goto("https://www.lambdatest.com/selenium-playground/input-form-demo")
 
         page.on('dialog', async dialog => { await dialog.accept() })
-              await page.getByRole('button', { name: "Allow all" }).click();
-      
-        
-        
+        await page.getByRole('button', { name: "Allow all" }).click();
+
+
+
         await page.locator('#name').fill('Evc')
         await page.locator('[class="w-full border border-gray-90 text-size-14 rounded mt-10 px-10 py-5"][type="email"]')
-            .pressSequentially('S@ch.com', { delay: 500 })      
-          
-        
-       
+            .pressSequentially('S@ch.com', { delay: 500 })
+
+
+
         await page.locator('input[placeholder="Password"]').fill('test123')
         await page.locator('[for="companyname"]~input[placeholder="Company"]').fill('LambdaTest')
         await page.selectOption('select[name="country"]', { label: 'Germany' })
         await page.locator('label:has-text("City*") ~ input#inputCity').fill('Freiburg')
-       await page.getByPlaceholder('Address 1').fill('Himmel Strasse 11')
-       await page.getByRole('textbox', { name: 'Address 2' }).fill('Am Wolke 7')
-       await page.getByRole('textbox', { name: 'State*' }).fill('NRW')
-       await page.getByRole('textbox', { name: 'Zip Code*' }).fill('51674')
+        await page.getByPlaceholder('Address 1').fill('Himmel Strasse 11')
+        await page.getByRole('textbox', { name: 'Address 2' }).fill('Am Wolke 7')
+        await page.getByRole('textbox', { name: 'State*' }).fill('NRW')
+        await page.getByRole('textbox', { name: 'Zip Code*' }).fill('51674')
     })
 })
