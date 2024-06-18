86e11d8ac988b68ef77d87b8e4d276e00fe6d5e8
e-nie <eniehaus111@gmail.com>
2024-06-18 17:00:42 +0200

    add pageObject DemoQA, datePicker spec

---
+++ tests/demoQA/datePickerDemoQA.spec.ts
@@ -0,0 +1,14 @@
+import { test as it } from 'playwright/test';
+import { DatePicker } from '../../page_object/demoQA/widgets/DatePickerDemoQA';
+
+
+it.describe('Date Picker for demoQA', () => {
+    it('working with date picker', async ({ page }) => {
+        let datePicker = new DatePicker(page);
+        await page.goto('https://demoqa.com/date-picker');
+
+        await datePicker.verifyHeader();
+
+    });
+
+});
