a2f54e3d981e1c1da832e9270ddb6658a1a63e94
e-nie <99044956+e-nie@users.noreply.github.com>
2024-06-21 17:24:22 +0200

    add authentication procedure (#9)

---
+++ tests/auth.setup.ts
@@ -0,0 +1,25 @@
+import { test as setup, expect } from '@playwright/test';
+
+const authFile = './.auth/user.json';
+
+setup('authenticate', async ({ page }) => {
+    // Perform authentication steps. Replace these actions with your own.
+    await page.goto('https://demoqa.com/login');
+    await page.getByPlaceholder('Username').fill('EvchenNie');
+    await page.getByPlaceholder('Password').fill('Evchen!1108');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Wait until the page receives the cookies.
+    //
+    // Sometimes login flow sets cookies in the process of several redirects.
+    // Wait for the final URL to ensure that the cookies are actually set.
+    await page.waitForURL('https://demoqa.com/profile');
+    await page.screenshot({ path: './screen/authenticate.png' });
+    // Alternatively, you can wait until the page reaches a state where all cookies are set.
+
+    await expect(page.locator('#userName-value')).toHaveText('EvchenNie');
+
+    // End of authentication steps.
+
+    await page.context().storageState({ path: authFile });
+});
