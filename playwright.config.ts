718451e147edbdd56e5b7c8dbba2123cf4f0c35b
e-nie <99044956+e-nie@users.noreply.github.com>
2024-06-22 17:41:57 +0200

    add assertion (#11)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
+import { config as dotenvConfig } from 'dotenv';
 
-dotenv.config({
+dotenvConfig({
   path: `./env/.env.${process.env.ENV}`
 })
 
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -31,10 +31,17 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: process.env.BASEURL || 'http://127.0.0.1:3000',
 
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'on-first-retry',
   },
 
+  expect: {
+    /* Set a higher timeout for the expect() assertions. */
+    timeout: 5000,
+  },
+  testMatch: 'test.list.ts',
   /* Configure projects for major browsers */
   projects: [
     { name: 'setup', testMatch: /.*\.setup\.ts/ },
---
+++ tests/lambdatest/test.list.ts
@@ -0,0 +1,3 @@
+import './assertion.spec.ts'
+import './dragAndDrop.spec.ts'
+import './datePicker.spec.ts'
