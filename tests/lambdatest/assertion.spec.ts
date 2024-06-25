f365b2601776f7e9c2beaec59af76febfef325c4
e-nie <eniehaus111@gmail.com>
2024-06-25 17:50:11 +0200

    fix assertion test

--- tests/lambdatest/assertion.spec.ts
+++ tests/lambdatest/assertion.spec.ts
@@ -13,7 +13,7 @@ test.describe('ASSERTION', () => {
         // await expect(page).toHaveTitle('Next - Generation Mobile Apps and Cross Browser Testing Platform | LambdaTest')
         const title = await page.title()
         console.log(title)
-        expect.soft(title).toContain('LambdaTests')
+        expect.soft(title).toContain('LambdaTest')
         const header = page.locator('h1')
         expect(await header.textContent()).toBe('Next-Generation Mobile Apps and Cross Browser Testing Cloud')
         await expect(page.locator('img[src$="Vimeo.svg"]')).toHaveAttribute('alt', 'vimeo', { timeout: 1000 })
