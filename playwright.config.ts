21a7232a2571bc5355e97dbe5788e418eaab11be
e-nie <eniehaus111@gmail.com>
2024-06-25 17:11:04 +0200

    change node version in yml file

--- playwright.config.ts
+++ playwright.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
     {
       name: 'chromium',
       use: {
-        ...devices['Desktop Chrome'], headless: false,
+        ...devices['Desktop Chrome'], headless: true,
         // storageState: './.auth/user.json'
       },
       // dependencies: ['setup'],
