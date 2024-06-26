a7eb4580576b7cbe9fe4ce142a7b64e9e0f75f57
e-nie <eniehaus111@gmail.com>
2024-06-26 19:40:36 +0200

    add testDemo yml file

--- tests/project/dropDown.spec.ts
+++ tests/project/dropDown.spec.ts
@@ -5,7 +5,7 @@ import { Dropdown } from "../../page_object/project/Dropdown";
 it.describe("DROPDOWN", () => {
     it("should interact with dropdown", async ({ page }) => {
         let dropdown = new Dropdown(page);
-        await page.goto('http://localhost:4200/pages/iot-dashboard')
+        await page.goto('http://localhost:4200')
         await dropdown.interactWithDropdown();
     })
 });
